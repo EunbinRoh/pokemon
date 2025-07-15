3f45eaeb2818e64f29d2a68ee987420d27ac785d
EunbinRoh <reb0525@naver.com>
2025-07-15 14:32:27 +0900

    Create pokemon.py

---
+++ pokemon.py
@@ -0,0 +1,39 @@
+import selenium.webdriver as wb
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+url_pokemon = "https://pokemonkorea.co.kr/pokedex"
+driver = wb.Chrome(options=options)
+driver.maximize_window()
+driver.get(url_pokemon)
+img = driver.find_element(By.CSS_SELECTOR,"img.img-fluid")
+img.click()
+time.sleep(1)
+
+h3 = driver.find_element(By.TAG_NAME,"h3")
+name = h3.text.split("\n")[1]
+
+driver.close()
+
+import csv
+import os
+
+pokemon_exist = os.path.exists("pokemon.ccsv")
+header = ["no","name"]
+
+with open("pokemon.csv","a") as file:
+    writer = csv.writer(file)
+
+    if not pokemon_exist :
+        writer.writerow(header)
+
+    writer.writerow(["0001",name])
+    print("포케몬 저장 완료!")
